c0eadd596c7a934e95339d14b43f14df0ef62747
Masatoshi Katabuchi <mattocci27@gmail.com>
2025-11-12 07:25:40 +0000

    use hydrate

--- R/packages.R
+++ R/packages.R
@@ -1,2 +1,23 @@
-library(httpgd)
-library(languageserver)
+#!/usr/bin/env Rscript
+
+if (!requireNamespace("renv", quietly = TRUE)) {
+  stop("renv must be available before hydrating project packages.")
+}
+
+base_library <- "/usr/local/lib/R/site-library"
+
+packages <- c(
+  "remotes",
+  "renv",
+  "devtools",
+  "yaml",
+  "languageserver",
+  # GitHub remotes are resolved via renv.lock; hydrate just needs package names.
+  "unigd",
+  "httpgd"
+)
+
+renv::hydrate(
+  packages = packages,
+  library = base_library
+)
